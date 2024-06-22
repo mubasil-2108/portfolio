cd47edf9ec8085acd798af810728899e27270c90
mubasil-2108 <mubasilbehzad234@gmail.com>
2024-06-23 00:36:02 +0500

    3rd Commit

--- src/components/footer/FooterBottom.js
+++ src/components/footer/FooterBottom.js
@@ -4,7 +4,7 @@ const FooterBottom = () => {
   return (
     <div className="w-full py-10">
       <p className="text-center text-gray-500 text-base">
-        © 2024. All rights reserved by Noor Mohammad
+        © 2024. All rights reserved by Mubasil Behzad
       </p>
     </div>
   );
